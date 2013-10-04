fe592c712d92dd40791b13099a75bc5e2082de63
laura <laura.bledaite@gmail.com>
2013-10-04 11:26:56 +0200

    Add sql for movie pairs

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -4,12 +4,8 @@ create table moviePairs (
 	movie1_id                 varchar(255) not null,
 	movie2_id                 varchar(255) not null,
 	constraint pk_moviePairs primary key (movie1_id, movie2_id))
+as select * from csvread('data/moviePairs.dat', 'fieldSeparator=,')
 ;
-
-insert into moviePairs values('2710', '231');
-insert into moviePairs values('2700', '2712');
-insert into moviePairs values('2427', '1721');
-insert into moviePairs values('1676', '288');
 	
 # --- !Downs
 
