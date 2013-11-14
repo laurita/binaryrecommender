3f8157e69b021995ccf8cfbd38c59722f67ec305
laura <laura.bledaite@gmail.com>
2013-11-14 14:15:33 +0100

    Add star images instead of large font * symbol in tooltip; change text() to html() in tooltip

--- public/bootstrap-slider/js/bootstrap-slider.js
+++ public/bootstrap-slider/js/bootstrap-slider.js
@@ -177,14 +177,14 @@
 				this.selectionElStyle.width = Math.abs(this.percentage[0] - this.percentage[1]) +'%';
 			}
 			if (this.range) {
-				this.tooltipInner.text(
+				this.tooltipInner.html(
 					this.formater(this.value[0]) + 
 					' : ' + 
 					this.formater(this.value[1])
 				);
 				this.tooltip[0].style[this.stylePos] = this.size * (this.percentage[0] + (this.percentage[1] - this.percentage[0])/2)/100 - (this.orientation === 'vertical' ? this.tooltip.outerHeight()/2 : this.tooltip.outerWidth()/2) +'px';
 			} else {
-				this.tooltipInner.text(
+				this.tooltipInner.html(
 					this.formater(this.value[0])
 				);
 				this.tooltip[0].style[this.stylePos] = this.size * this.percentage[0]/100 - (this.orientation === 'vertical' ? this.tooltip.outerHeight()/2 : this.tooltip.outerWidth()/2) +'px';
